37e78f91949be8ea8b6bdd285cc1e68fdffcdfed
Tatu Anttonen <tatu.anttonen@gmail.com>
2021-10-16 20:28:36 +0300

    Electron 15.2.0 and upgraded webpack build tool to version 5

--- config/electron.webpack.config.js
+++ config/electron.webpack.config.js
@@ -1,28 +1,18 @@
-// `CheckerPlugin` is optional. Use it if you want async error reporting.
-// We need this plugin to detect a `--watch` mode. It may be removed later
-// after https://github.com/webpack/webpack/issues/3460 will be resolved.
-// const { CheckerPlugin } = require('awesome-typescript-loader')
 const path = require("path")
 
 module.exports = env => {
 
-	const common = require('./common.webpack.config')
-	const {plugins, sourceMapLoader, devtool} = common(env)
-
 	return {
 		entry: './src/electron/index.ts',
 		output: {
-			// path: path.resolve(__dirname, '../'),
 			filename: 'electron-renderer.bundle.js'
 		},
 		target: 'electron-renderer',
 		resolve: {
 			extensions: ['.ts', '.js']
 		},
-		devtool,
 		module: {
 			rules: [
-				sourceMapLoader,
 				{
 					test: /\.ts$/,
 					loader: "ts-loader",
@@ -33,6 +23,5 @@ module.exports = env => {
 				}
 			]
 		},
-		plugins
 	}
 }
